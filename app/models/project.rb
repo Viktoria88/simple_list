33d568c9d33950810bcd58fd19ba3a5eb572b508
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-08-28 20:20:15 +0300

    Delete dependent: :destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
 
-  has_many :tasks, inverse_of: :project, :dependent => :destroy
+  has_many :tasks, inverse_of: :project
   accepts_nested_attributes_for :tasks, reject_if: :new_record?, allow_destroy: true
 end
