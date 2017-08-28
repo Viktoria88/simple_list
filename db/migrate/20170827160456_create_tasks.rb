dedf8de89c0dcb5556e261b77b3e584fb3a299fe
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-08-28 21:40:17 +0300

    Some change

--- db/migrate/20170827160456_create_tasks.rb
+++ db/migrate/20170827160456_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :done
+      t.boolean :done, default: false, null: false
       t.datetime :deadline
       t.references :project, foreign_key: true
 
