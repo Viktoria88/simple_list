f00a633b1bbda229bca539c10320ddff26abce36
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-08-28 19:44:58 +0300

    Fixed :dependent => :destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
 
-  has_many :tasks, inverse_of: :project
+  has_many :tasks, inverse_of: :project, :dependent => :destroy
   accepts_nested_attributes_for :tasks, reject_if: :new_record?, allow_destroy: true
 end
